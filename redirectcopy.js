cebabeb6ddffc0b48a9b1888afd9d9a06f8d07a8
hoangnguyen888 <56681410+hoangnguyen888@users.noreply.github.com>
2019-10-17 21:31:47 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://nguyenttcland999.blogspot.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://nguyenttcland999.blogspot.com'>https://nguyenttcland999.blogspot.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
